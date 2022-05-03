bece535fd5378717473699023725d2d9a6d52e51
lucasfarmwald <farmwaldlucas@gmail.com>
2022-05-03 13:18:37 -0500

    final commit

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
 
   attr_accessor :name, :album, :id
 
-  def initialize(name:, album:, id: )
+  def initialize(name:, album:, id: nil )
     @id = id
     @name = name
     @album = album
@@ -17,5 +17,20 @@ class Song
       SQL
     DB[:conn].execute(sql)
   end
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, album)
+      VALUES (?,?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.album)
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+
+    self
+    end
+    def self.create(name:, album:)
+      song = Song.new(name: name, album: album)
+      song.save
+    end
 
 end
